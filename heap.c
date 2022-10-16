c8db2cc9d852255e758b4cdcf481ce48ac2bb7c9
Replit user <>
2022-10-16 23:42:57 +0000

    new try

--- heap.c
+++ heap.c
@@ -49,7 +49,7 @@ void heap_pop(Heap* pq){
   now = 1;
 
   while ((now<=pq->size && pq->heapArray[now].priority > priority)||now+1<=pq->size&&pq->heapArray[now+1].priority > priority){
-    heapElem temporal = heapArray[(now+1)/2];
+    heapElem temporal = pq->heapArray[(now+1)/2];
     if(no+1<=pq->size && pq->heapArray[now+1].priority > priority) now++;
     pq->heapArray[(now-1)/2] = pq->heapArray[now];
     pq->heapArray[now]= temporal;
